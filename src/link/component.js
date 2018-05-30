bd6d06e6f7fc50d2c7dca22f51f4debb3f61f7ad
SnejUgal <snejugal@icloud.com>
2018-05-31 00:11:53 +0700

    link component

---
+++ src/link/component.js
@@ -0,0 +1,40 @@
+import "./styles.scss";
+
+import PropTypes from "prop-types";
+import React from "react";
+
+class Link extends React.Component {
+  static propTypes = {
+    href: PropTypes.string.isRequired,
+    children: PropTypes.any,
+    isWhite: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    isWhite: false,
+  }
+
+  stopPropagation = (event) => event.stopPropagation()
+
+  render () {
+    let className = `link`;
+
+    if (this.props.isWhite) {
+      className += ` -white`;
+    }
+
+    return (
+      <a
+        className={className}
+        href={this.props.href}
+        onClick={this.stopPropagation}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+export default Link;
