d386435c716bd292b9fadad0569651cb61a00fba
SnejUgal <snejugal@icloud.com>
2018-05-31 00:12:43 +0700

    refactoriation and strings for empty workplace

--- src/localizations/en.js
+++ src/localizations/en.js
@@ -1,23 +1,20 @@
+import Link from "../link/component";
 import React from "react";
-
-const stopPropagation = (event) => event.stopPropagation();
 
 const localization = {
   error_title: () => `Whoops, some error happened.`,
   error_description: () => <React.Fragment>
-    Please send a screenshot of the following error to
-    <a
-      className="link -white"
-      href="//t.me/snejugal"
-      target="_blank"
-      rel="noopener noreferrer"
-      onClick={stopPropagation}
-    >
+    Please send a screenshot of the following error to {}
+    <Link href="//t.me/snejugal" isWhite={true}>
       the developer on Telegram
-    </a>
-    and describe what you did so the error happened. You may also need to send the original theme file you used.
+    </Link>
+    {} and describe what you did so the error happened. You may also need to send the original theme file you used.
   </React.Fragment>,
-  error_dismiss: () => `To dissmiss the error, tap it.`,
+  error_dismiss: () => `To dismiss the error, just tap it.`,
+
+  emptyWorkspace_title: () => `Start working on your theme`,
+  emptyWorkspace_createTheme: () => `Create a new theme`,
+  emptyWorkspace_openTheme: () => `Open an existing theme`,
 };
 
 export default localization;
--- src/localizations/ru.js
+++ src/localizations/ru.js
@@ -1,25 +1,22 @@
+import Link from "../link/component";
 import React from "react";
 
 const NBSP = `\xa0`;
-
-const stopPropagation = (event) => event.stopPropagation();
 
 const localization = {
   error_title: () => `Упс, произошла ошибка`,
   error_description: () => <React.Fragment>
     Пожалуйста, отправьте скриншот ошибки {}
-    <a
-      className="link -white"
-      href="//t.me/snejugal"
-      target="_blank"
-      rel="noopener noreferrer"
-      onClick={stopPropagation}
-    >
+    <Link href="//t.me/snejugal" isWhite={true}>
       разработчику по&nbsp;Телеграму
-    </a>
+    </Link>
     {} и&nbsp;опишите, что&nbsp;вы&nbsp;сделали, после чего возникла ошибка. Возможно, понадобится отправить и&nbsp;оригинальный файл темы, который вы&nbsp;использовали.
   </React.Fragment>,
-  error_dismiss: () => `Чтобы закрыть сообщение об${NBSP}ошибке, нажмите него.`,
+  error_dismiss: () => `Чтобы закрыть сообщение об${NBSP}ошибке, просто нажмите на него.`,
+
+  emptyWorkspace_title: () => `Начните работать над своей темой`,
+  emptyWorkspace_createTheme: () => `Создать новую тему`,
+  emptyWorkspace_openTheme: () => `Открыть существующую тему`,
 };
 
 export default localization;
