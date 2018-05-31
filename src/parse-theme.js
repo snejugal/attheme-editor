2d949dab562f4a3fccfde8be36b49ac5b0e4a7e4
SnejUgal <snejugal@icloud.com>
2018-05-31 12:50:30 +0700

    theme parser

--- src/parse-theme.js
+++ src/parse-theme.js
@@ -1,1 +1,34 @@
-// TODO
+import { basename, extname } from "path";
+import Attheme from "attheme-js";
+
+const parseTheme = ({ file, filename }) => {
+  let theme;
+  let wallpaper;
+  let palette;
+  let name;
+
+  const extension = extname(filename);
+
+  if (extension === `.attheme`) {
+    theme = new Attheme(file);
+
+    wallpaper = btoa(theme[Attheme.IMAGE_KEY] || ``);
+    name = basename(filename, extension);
+    palette = [];
+
+    delete theme[Attheme.IMAGE_KEY];
+  } else if (extension === `.attheme-editor`) {
+    const json = JSON.parse(file);
+
+    ({ theme, wallpaper, palette, name } = json);
+  }
+
+  return {
+    theme,
+    wallpaper,
+    palette,
+    name,
+  };
+};
+
+export default parseTheme;
