ef91de29eea2b615e6dff6501600e708e6e961c4
SnejUgal <snejugal@icloud.com>
2018-06-01 13:24:12 +0700

    confirmDialog component

---
+++ src/confirm-dialog/component.js
@@ -0,0 +1,35 @@
+import Button from "../button/component";
+import Dialog from "../dialog/component";
+import PropTypes from "prop-types";
+import React from "react";
+import localization from "../localization";
+
+class ConfirmDialog extends React.Component {
+  static propTypes = {
+    children: PropTypes.any,
+    onDismissed: PropTypes.func.isRequired,
+    onConfirmed: PropTypes.func.isRequired,
+  }
+
+  render () {
+    return (
+      <Dialog
+        onDismiss={this.props.onDismissed}
+        buttons={
+          <React.Fragment>
+            <Button onClick={this.props.onConfirmed}>
+              {localization.confirmDialog_yes()}
+            </Button>
+            <Button onClick={this.props.onDismissed}>
+              {localization.confirmDialog_no()}
+            </Button>
+          </React.Fragment>
+        }
+      >
+        {this.props.children}
+      </Dialog>
+    );
+  }
+}
+
+export default ConfirmDialog;
