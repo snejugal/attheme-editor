5dda9390cd2060e337bd2b9ceb60e0876fc70c92
SnejUgal <snejugal@icloud.com>
2018-06-02 21:03:44 +0700

    hint component

---
+++ src/hint/component.js
@@ -0,0 +1,16 @@
+import "./styles.scss";
+
+import PropTypes from "prop-types";
+import React from "react";
+
+class Hint extends React.Component {
+  static propTypes = {
+    children: PropTypes.any,
+  }
+
+  render () {
+    return <p className="paragraph hint">{this.props.children}</p>;
+  }
+}
+
+export default Hint;
