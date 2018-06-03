b069a59d7ef4c095ff67e635c105e20b37343d49
SnejUgal <snejugal@icloud.com>
2018-06-03 18:05:12 +0700

    remove lowecaseVariables

--- src/attheme-variables.js
+++ src/attheme-variables.js
@@ -1,11 +1,9 @@
 import defaultValues from "attheme-default-values";
 
 const variables = Object.keys(defaultValues);
-const lowercaseVariables = variables.map((x) => x.toLowerCase());
 const allVariablesAmount = variables.length;
 
 export {
   variables,
-  lowercaseVariables,
   allVariablesAmount,
 };
