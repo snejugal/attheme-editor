b0f0d3cd26b641784ac0a2c2d5c9ff1e889af282
SnejUgal <snejugal@icloud.com>
2018-06-04 15:48:26 +0700

    unused, obsolete variables and list of default values

--- src/attheme-variables.js
+++ src/attheme-variables.js
@@ -1,9 +1,32 @@
-import defaultValues from "attheme-default-values";
+import allVariablesValues from "attheme-default-values";
 
-const variables = Object.keys(defaultValues);
-const allVariablesAmount = variables.length;
+const OBSOLETE_VARIABLES = [
+  `listSelector`,
+  `player_seekBarBackground`,
+  `player_duration`,
+];
+
+const UNUSED_VARIABLES = [
+  `chat_mediaBroadcast`,
+  `chat_outBroadcast`,
+  `windowBackgroundWhiteBlueText2`,
+];
+
+const defaultValues = {
+  ...allVariablesValues,
+};
+
+for (const unusedVariable of UNUSED_VARIABLES) {
+  delete defaultValues[unusedVariable];
+}
+
+const allVariables = Object.keys(defaultValues);
+const allVariablesAmount = allVariables.length;
 
 export {
-  variables,
+  OBSOLETE_VARIABLES,
+  UNUSED_VARIABLES,
+  allVariables,
   allVariablesAmount,
+  defaultValues,
 };
