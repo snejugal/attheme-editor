a0b3e10bc3d8bb65ea4ba83ebd82ead5794651ba
SnejUgal <snejugal@icloud.com>
2018-06-18 21:24:04 +0700

    provide rgbToHsl, HslToRb in scripts

--- src/script-runner/color-class.js
+++ src/script-runner/color-class.js
@@ -3,8 +3,13 @@
 import Color from "../color";
 
 const RGBA_CHANNELS = [`red`, `green`, `blue`, `alpha`];
+const HSLA_CHANNELS = [`hue`, `saturation`, `lightness`, `alpha`];
 const MIN_RGBA_CHANNEL_VALUE = 0;
 const MAX_RGBA_CHANNEL_VALUE = 255;
+const MIN_HUE_VALUE = 0;
+const MAX_HUE_VALUE_EXCLUDING = 360;
+const MIN_SATURATION_LIGHTNESS_VALUE = 0;
+const MAX_SATURATION_LIGHTNESS_VALUE = 1;
 
 const checkRgbColorValidity = (rgbColor) => {
   if (typeof rgbColor !== `object`) {
@@ -31,9 +36,55 @@ const checkRgbColorValidity = (rgbColor) => {
       || rgbColor[channel] < MIN_RGBA_CHANNEL_VALUE
       || rgbColor[channel] % 1 !== 0
     ) {
-      throw new TypeError(`The ${channel} channel is out of range 0..255 (received ${rgbColor[channel]})`);
+      throw new TypeError(`The ${channel} channel is out of the range [0, 255] (received ${rgbColor[channel]})`);
     }
   });
+};
+
+const checkHslColorValidity = (hslColor) => {
+  if (typeof hslColor !== `object`) {
+    throw new TypeError(`Expected the passed color to be an object, received ${typeof hslColor}`);
+  }
+
+  for (const channel in hslColor) {
+    if (!HSLA_CHANNELS.includes(channel)) {
+      throw new TypeError(`Unknown "${channel}" channel in the passed RGB color`);
+    }
+  }
+
+  HSLA_CHANNELS.forEach((channel) => {
+    if (!(channel in hslColor)) {
+      throw new TypeError(`The ${channel} channel was not specified in the passed HSL color`);
+    }
+
+    if (typeof hslColor[channel] !== `number`) {
+      throw new TypeError(`The ${channel} channel is not a number (received ${typeof hslColor[channel]})`);
+    }
+  });
+
+  if (
+    hslColor.hue >= MAX_HUE_VALUE_EXCLUDING
+    || hslColor.hue < MIN_HUE_VALUE
+  ) {
+    throw new TypeError(`The hue channel is out of the range [0.0, 360.0) (received ${hslColor.hue})`);
+  }
+
+  if (
+    hslColor.alpha > MAX_RGBA_CHANNEL_VALUE
+    || hslColor.alpha < MIN_RGBA_CHANNEL_VALUE
+    || hslColor.alpha % 1 !== 0
+  ) {
+    throw new TypeError(`The alpha channel is out of the range [0, 255] (received ${hslColor.alpha})`);
+  }
+
+  for (const channel of [`saturation`, `lightness`]) {
+    if (
+      hslColor[channel] > MAX_SATURATION_LIGHTNESS_VALUE
+      || hslColor[channel] < MIN_SATURATION_LIGHTNESS_VALUE
+    ) {
+      throw new TypeError(`The ${channel} channel is out of the range [0.0, 1.0] (received ${hslColor[channel]})`);
+    }
+  }
 };
 
 const normalizeRgbColor = (rgbColor, ...excessiveArguments) => {
@@ -96,6 +147,97 @@ const normalizeRgbColor = (rgbColor, ...excessiveArguments) => {
   return fixedColor;
 };
 
+const normalizeHslColor = (hslColor, ...excessiveArguments) => {
+  if (excessiveArguments.length > 0) {
+    throw new TypeError(`Color.normalizeHslColor expected 1 argument, received ${excessiveArguments.length + 1}`);
+  }
+
+  if (typeof hslColor !== `object`) {
+    throw new TypeError(`Expected the passed color to be an object, received ${typeof hslColor}`);
+  }
+
+  const fixedColor = {
+    ...hslColor,
+  };
+
+  for (const channel in fixedColor) {
+    if (!HSLA_CHANNELS.includes(channel)) {
+      delete fixedColor[channel];
+    }
+  }
+
+  for (const channel of HSLA_CHANNELS) {
+    if (!(channel in fixedColor)) {
+      if (channel === `alpha`) {
+        fixedColor.alpha = 255;
+      } else {
+        throw new TypeError(`The ${channel} channel was not specified in the passed HSL color`);
+      }
+    }
+
+    if (typeof fixedColor[channel] !== `number`) {
+      try {
+        fixedColor[channel] = Number(fixedColor[channel]);
+      } catch (error) {
+        try {
+          fixedColor[channel] = Number(String(fixedColor[channel]));
+        } catch (secondError) {
+          throw new TypeError(`Could not convert ${fixedColor[channel]} to a number`);
+        }
+      }
+    }
+  }
+
+  if (Number.isNaN(fixedColor.alpha)) {
+    fixedColor.alpha = MIN_RGBA_CHANNEL_VALUE;
+  }
+
+  if (fixedColor.alpha > MAX_RGBA_CHANNEL_VALUE) {
+    fixedColor.alpha = MAX_RGBA_CHANNEL_VALUE;
+  }
+
+  if (fixedColor.alpha < MIN_RGBA_CHANNEL_VALUE) {
+    fixedColor.alpha = MIN_RGBA_CHANNEL_VALUE;
+  }
+
+  if (fixedColor.alpha % 1 !== 0) {
+    fixedColor.alpha = Math.round(fixedColor.alpha);
+  }
+
+  if (Number.isNaN(fixedColor.hue)) {
+    fixedColor.hue = MIN_HUE_VALUE;
+  }
+
+  if (fixedColor.hue >= MAX_HUE_VALUE_EXCLUDING) {
+    fixedColor.hue %= MAX_HUE_VALUE_EXCLUDING;
+  }
+
+  if (fixedColor.hue < MIN_HUE_VALUE) {
+    fixedColor.hue =
+      (
+        MAX_HUE_VALUE_EXCLUDING
+        + (fixedColor.hue % MAX_HUE_VALUE_EXCLUDING)
+      )
+      % MAX_HUE_VALUE_EXCLUDING;
+  }
+
+  for (const channel of [`saturation`, `lightness`]) {
+    if (Number.isNaN(fixedColor[channel])) {
+      fixedColor[channel] = MIN_SATURATION_LIGHTNESS_VALUE;
+    }
+
+    if (fixedColor[channel] > MAX_SATURATION_LIGHTNESS_VALUE) {
+      fixedColor[channel] = MAX_SATURATION_LIGHTNESS_VALUE;
+    }
+
+    if (fixedColor[channel] < MIN_SATURATION_LIGHTNESS_VALUE) {
+      fixedColor[channel] = MIN_SATURATION_LIGHTNESS_VALUE;
+    }
+  }
+
+  return fixedColor;
+};
+
 const brightness = (color, ...excessiveArguments) => {
   if (excessiveArguments.length > 0) {
     throw new TypeError(`Color.brightness expected 1 argument, received ${excessiveArguments.length + 1}`);
@@ -128,6 +270,26 @@ const createHex = (color, ...excessiveArguments) => {
   return Color.createHex(color);
 };
 
+const rgbToHsl = (color, ...excessiveArguments) => {
+  if (excessiveArguments.length > 0) {
+    throw new TypeError(`Color.rgbToHsl expected 1 argument, received ${excessiveArguments.length + 1}`);
+  }
+
+  checkRgbColorValidity(color);
+
+  return Color.rgbToHsl(color);
+};
+
+const hslToRgb = (color, ...excessiveArguments) => {
+  if (excessiveArguments.length > 0) {
+    throw new TypeError(`Color.hslToRgb expected 1 argument, received ${excessiveArguments.length + 1}`);
+  }
+
+  checkHslColorValidity(color);
+
+  return Color.hslToRgb(color);
+};
+
 const overlay = (...colors) => {
   for (const color of colors) {
     checkRgbColorValidity(color);
@@ -138,10 +300,13 @@ const overlay = (...colors) => {
 
 const colorClass = {
   normalizeRgbColor,
+  normalizeHslColor,
   brightness,
   parseHex,
   createHex,
   overlay,
+  rgbToHsl,
+  hslToRgb,
 };
 
 export default colorClass;
