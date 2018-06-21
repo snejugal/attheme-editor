79d48d42a29743d5a9a0bba4826c485b3970d6e3
SnejUgal <snejugal@icloud.com>
2018-06-21 17:46:04 +0700

    material design palette

---
+++ src/palettes/built-in-palettes/material-design.js
@@ -0,0 +1,84 @@
+import Color from "../../color";
+
+const palette = [
+  {
+    name: `Red 500`,
+    color: Color.parseHex(`#f44336`),
+  },
+  {
+    name: `Pink 500`,
+    color: Color.parseHex(`#e91e63`),
+  },
+  {
+    name: `Purple 500`,
+    color: Color.parseHex(`#9c27b0`),
+  },
+  {
+    name: `Deep Purple 500`,
+    color: Color.parseHex(`#673ab7`),
+  },
+  {
+    name: `Indigo 500`,
+    color: Color.parseHex(`#3f51b5`),
+  },
+  {
+    name: `Blue 500`,
+    color: Color.parseHex(`#2196f3`),
+  },
+  {
+    name: `Light Blue 500`,
+    color: Color.parseHex(`#03a9f4`),
+  },
+  {
+    name: `Cyan 500`,
+    color: Color.parseHex(`#00bcd4`),
+  },
+  {
+    name: `Teal 500`,
+    color: Color.parseHex(`#009688`),
+  },
+  {
+    name: `Green 500`,
+    color: Color.parseHex(`#4caf50`),
+  },
+  {
+    name: `Light Green 500`,
+    color: Color.parseHex(`#8bc34a`),
+  },
+  {
+    name: `Lime 500`,
+    color: Color.parseHex(`#cddc39`),
+  },
+  {
+    name: `Yellow 500`,
+    color: Color.parseHex(`#ffeb3b`),
+  },
+  {
+    name: `Amber 500`,
+    color: Color.parseHex(`#ffc107`),
+  },
+  {
+    name: `Orange 500`,
+    color: Color.parseHex(`#ff9800`),
+  },
+  {
+    name: `Deep Orange 500`,
+    color: Color.parseHex(`#ff5722`),
+  },
+  {
+    name: `Brown 500`,
+    color: Color.parseHex(`#795548`),
+  },
+  {
+    name: `Gray 500`,
+    color: Color.parseHex(`#9e9e9e`),
+  },
+  {
+    name: `Blue Gray 500`,
+    color: Color.parseHex(`#607d8b`),
+  },
+];
+
+export {
+  palette,
+};
