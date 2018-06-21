5f7bbaaa824e0beae23e8745834a469646cad51e
SnejUgal <snejugal@icloud.com>
2018-06-21 17:45:54 +0700

    css palette

---
+++ src/palettes/built-in-palettes/css.js
@@ -0,0 +1,564 @@
+import Color from "../../color";
+
+const palette = [
+  {
+    name: `aliceblue`,
+    color: Color.parseHex(`#f0f8ff`),
+  },
+  {
+    name: `antiquewhite`,
+    color: Color.parseHex(`#faebd7`),
+  },
+  {
+    name: `aquamarine`,
+    color: Color.parseHex(`#7fffd4`),
+  },
+  {
+    name: `azure`,
+    color: Color.parseHex(`#f0ffff`),
+  },
+  {
+    name: `beige`,
+    color: Color.parseHex(`#f5f5dc`),
+  },
+  {
+    name: `bisque`,
+    color: Color.parseHex(`#ffe4c4`),
+  },
+  {
+    name: `black`,
+    color: Color.parseHex(`#000000`),
+  },
+  {
+    name: `blanchedalmond`,
+    color: Color.parseHex(`#ffebcd`),
+  },
+  {
+    name: `blue`,
+    color: Color.parseHex(`#0000ff`),
+  },
+  {
+    name: `blueviolet`,
+    color: Color.parseHex(`#8a2be2`),
+  },
+  {
+    name: `brown`,
+    color: Color.parseHex(`#a52a2a`),
+  },
+  {
+    name: `burlywood`,
+    color: Color.parseHex(`#deb887`),
+  },
+  {
+    name: `cadetblue`,
+    color: Color.parseHex(`#5f9ea0`),
+  },
+  {
+    name: `chartreuse`,
+    color: Color.parseHex(`#7fff00`),
+  },
+  {
+    name: `chocolate`,
+    color: Color.parseHex(`#d2691e`),
+  },
+  {
+    name: `coral`,
+    color: Color.parseHex(`#ff7f50`),
+  },
+  {
+    name: `cornflowerblue`,
+    color: Color.parseHex(`#6495ed`),
+  },
+  {
+    name: `cornsilk`,
+    color: Color.parseHex(`#fff8dc`),
+  },
+  {
+    name: `crimson`,
+    color: Color.parseHex(`#dc143c`),
+  },
+  {
+    name: `cyan`,
+    color: Color.parseHex(`#00ffff`),
+  },
+  {
+    name: `darkblue`,
+    color: Color.parseHex(`#00008b`),
+  },
+  {
+    name: `darkcyan`,
+    color: Color.parseHex(`#008b8b`),
+  },
+  {
+    name: `darkgoldenrod`,
+    color: Color.parseHex(`#b8860b`),
+  },
+  {
+    name: `darkgray`,
+    color: Color.parseHex(`#a9a9a9`),
+  },
+  {
+    name: `darkgreen`,
+    color: Color.parseHex(`#006400`),
+  },
+  {
+    name: `darkkhaki`,
+    color: Color.parseHex(`#bdb76b`),
+  },
+  {
+    name: `darkmagenta`,
+    color: Color.parseHex(`#8b008b`),
+  },
+  {
+    name: `darkolivegreen`,
+    color: Color.parseHex(`#556b2f`),
+  },
+  {
+    name: `darkorange`,
+    color: Color.parseHex(`#ff8c00`),
+  },
+  {
+    name: `darkorchid`,
+    color: Color.parseHex(`#9932cc`),
+  },
+  {
+    name: `darkred`,
+    color: Color.parseHex(`#8b0000`),
+  },
+  {
+    name: `darksalmon`,
+    color: Color.parseHex(`#e9967a`),
+  },
+  {
+    name: `darkseagreen`,
+    color: Color.parseHex(`#8fbc8f`),
+  },
+  {
+    name: `darkslateblue`,
+    color: Color.parseHex(`#483d8b`),
+  },
+  {
+    name: `darkslategray`,
+    color: Color.parseHex(`#2f4f4f`),
+  },
+  {
+    name: `darkturquoise`,
+    color: Color.parseHex(`#00ced1`),
+  },
+  {
+    name: `darkviolet`,
+    color: Color.parseHex(`#9400d3`),
+  },
+  {
+    name: `deeppink`,
+    color: Color.parseHex(`#ff1493`),
+  },
+  {
+    name: `deepskyblue`,
+    color: Color.parseHex(`#00bfff`),
+  },
+  {
+    name: `dimgray`,
+    color: Color.parseHex(`#696969`),
+  },
+  {
+    name: `dodgerblue`,
+    color: Color.parseHex(`#1e90ff`),
+  },
+  {
+    name: `firebrick`,
+    color: Color.parseHex(`#b22222`),
+  },
+  {
+    name: `floralwhite`,
+    color: Color.parseHex(`#fffaf0`),
+  },
+  {
+    name: `forestgreen`,
+    color: Color.parseHex(`#228b22`),
+  },
+  {
+    name: `gainsboro`,
+    color: Color.parseHex(`#dcdcdc`),
+  },
+  {
+    name: `ghostwhite`,
+    color: Color.parseHex(`#f8f8ff`),
+  },
+  {
+    name: `gold`,
+    color: Color.parseHex(`#ffd700`),
+  },
+  {
+    name: `goldenrod`,
+    color: Color.parseHex(`#daa520`),
+  },
+  {
+    name: `gray`,
+    color: Color.parseHex(`#808080`),
+  },
+  {
+    name: `green`,
+    color: Color.parseHex(`#008000`),
+  },
+  {
+    name: `greenyellow`,
+    color: Color.parseHex(`#adff2f`),
+  },
+  {
+    name: `honeydew`,
+    color: Color.parseHex(`#f0fff0`),
+  },
+  {
+    name: `hotpink`,
+    color: Color.parseHex(`#ff69b4`),
+  },
+  {
+    name: `indianred`,
+    color: Color.parseHex(`#cd5c5c`),
+  },
+  {
+    name: `indigo`,
+    color: Color.parseHex(`#4b0082`),
+  },
+  {
+    name: `ivory`,
+    color: Color.parseHex(`#fffff0`),
+  },
+  {
+    name: `khaki`,
+    color: Color.parseHex(`#f0e68c`),
+  },
+  {
+    name: `lavender`,
+    color: Color.parseHex(`#e6e6fa`),
+  },
+  {
+    name: `lavenderblush`,
+    color: Color.parseHex(`#fff0f5`),
+  },
+  {
+    name: `lawngreen`,
+    color: Color.parseHex(`#7cfc00`),
+  },
+  {
+    name: `lemonchiffon`,
+    color: Color.parseHex(`#fffacd`),
+  },
+  {
+    name: `lightblue`,
+    color: Color.parseHex(`#add8e6`),
+  },
+  {
+    name: `lightcoral`,
+    color: Color.parseHex(`#f08080`),
+  },
+  {
+    name: `lightcyan`,
+    color: Color.parseHex(`#e0ffff`),
+  },
+  {
+    name: `lightgoldenrodyellow`,
+    color: Color.parseHex(`#fafad2`),
+  },
+  {
+    name: `lightgray`,
+    color: Color.parseHex(`#d3d3d3`),
+  },
+  {
+    name: `lightgreen`,
+    color: Color.parseHex(`#90ee90`),
+  },
+  {
+    name: `lightpink`,
+    color: Color.parseHex(`#ffb6c1`),
+  },
+  {
+    name: `lightsalmon`,
+    color: Color.parseHex(`#ffa07a`),
+  },
+  {
+    name: `lightseagreen`,
+    color: Color.parseHex(`#20b2aa`),
+  },
+  {
+    name: `lightskyblue`,
+    color: Color.parseHex(`#87cefa`),
+  },
+  {
+    name: `lightslategray`,
+    color: Color.parseHex(`#778899`),
+  },
+  {
+    name: `lightsteelblue`,
+    color: Color.parseHex(`#b0c4de`),
+  },
+  {
+    name: `lightyellow`,
+    color: Color.parseHex(`#ffffe0`),
+  },
+  {
+    name: `lime`,
+    color: Color.parseHex(`#00ff00`),
+  },
+  {
+    name: `limegreen`,
+    color: Color.parseHex(`#32cd32`),
+  },
+  {
+    name: `linen`,
+    color: Color.parseHex(`#faf0e6`),
+  },
+  {
+    name: `magenta`,
+    color: Color.parseHex(`#ff00ff`),
+  },
+  {
+    name: `maroon`,
+    color: Color.parseHex(`#800000`),
+  },
+  {
+    name: `mediumaquamarine`,
+    color: Color.parseHex(`#66cdaa`),
+  },
+  {
+    name: `mediumblue`,
+    color: Color.parseHex(`#0000cd`),
+  },
+  {
+    name: `mediumorchid`,
+    color: Color.parseHex(`#ba55d3`),
+  },
+  {
+    name: `mediumpurple`,
+    color: Color.parseHex(`#9370db`),
+  },
+  {
+    name: `mediumseagreen`,
+    color: Color.parseHex(`#3cb371`),
+  },
+  {
+    name: `mediumslateblue`,
+    color: Color.parseHex(`#7b68ee`),
+  },
+  {
+    name: `mediumspringgreen`,
+    color: Color.parseHex(`#00fa9a`),
+  },
+  {
+    name: `mediumturquoise`,
+    color: Color.parseHex(`#48d1cc`),
+  },
+  {
+    name: `mediumvioletred`,
+    color: Color.parseHex(`#c71585`),
+  },
+  {
+    name: `midnightblue`,
+    color: Color.parseHex(`#191970`),
+  },
+  {
+    name: `mintcream`,
+    color: Color.parseHex(`#f5fffa`),
+  },
+  {
+    name: `mistyrose`,
+    color: Color.parseHex(`#ffe4e1`),
+  },
+  {
+    name: `moccasin`,
+    color: Color.parseHex(`#ffe4b5`),
+  },
+  {
+    name: `navajowhite`,
+    color: Color.parseHex(`#ffdead`),
+  },
+  {
+    name: `navy`,
+    color: Color.parseHex(`#000080`),
+  },
+  {
+    name: `oldlace`,
+    color: Color.parseHex(`#fdf5e6`),
+  },
+  {
+    name: `olive`,
+    color: Color.parseHex(`#808000`),
+  },
+  {
+    name: `olivedrab`,
+    color: Color.parseHex(`#6b8e23`),
+  },
+  {
+    name: `orange`,
+    color: Color.parseHex(`#ffa500`),
+  },
+  {
+    name: `orangered`,
+    color: Color.parseHex(`#ff4500`),
+  },
+  {
+    name: `orchid`,
+    color: Color.parseHex(`#da70d6`),
+  },
+  {
+    name: `palegoldenrod`,
+    color: Color.parseHex(`#eee8aa`),
+  },
+  {
+    name: `palegreen`,
+    color: Color.parseHex(`#98fb98`),
+  },
+  {
+    name: `paleturquoise`,
+    color: Color.parseHex(`#afeeee`),
+  },
+  {
+    name: `palevioletred`,
+    color: Color.parseHex(`#db7093`),
+  },
+  {
+    name: `papayawhip`,
+    color: Color.parseHex(`#ffefd5`),
+  },
+  {
+    name: `peachpuff`,
+    color: Color.parseHex(`#ffdab9`),
+  },
+  {
+    name: `peru`,
+    color: Color.parseHex(`#cd853f`),
+  },
+  {
+    name: `pink`,
+    color: Color.parseHex(`#ffc0cb`),
+  },
+  {
+    name: `plum`,
+    color: Color.parseHex(`#dda0dd`),
+  },
+  {
+    name: `powderblue`,
+    color: Color.parseHex(`#b0e0e6`),
+  },
+  {
+    name: `purple`,
+    color: Color.parseHex(`#800080`),
+  },
+  {
+    name: `rebeccapurple`,
+    color: Color.parseHex(`#663399`),
+  },
+  {
+    name: `red`,
+    color: Color.parseHex(`#ff0000`),
+  },
+  {
+    name: `rosybrown`,
+    color: Color.parseHex(`#bc8f8f`),
+  },
+  {
+    name: `royalblue`,
+    color: Color.parseHex(`#4169e1`),
+  },
+  {
+    name: `saddlebrown`,
+    color: Color.parseHex(`#8b4513`),
+  },
+  {
+    name: `salmon`,
+    color: Color.parseHex(`#fa8072`),
+  },
+  {
+    name: `sandybrown`,
+    color: Color.parseHex(`#f4a460`),
+  },
+  {
+    name: `seagreen`,
+    color: Color.parseHex(`#2e8b57`),
+  },
+  {
+    name: `seashell`,
+    color: Color.parseHex(`#fff5ee`),
+  },
+  {
+    name: `sienna`,
+    color: Color.parseHex(`#a0522d`),
+  },
+  {
+    name: `silver`,
+    color: Color.parseHex(`#c0c0c0`),
+  },
+  {
+    name: `skyblue`,
+    color: Color.parseHex(`#87ceeb`),
+  },
+  {
+    name: `slateblue`,
+    color: Color.parseHex(`#6a5acd`),
+  },
+  {
+    name: `slategray`,
+    color: Color.parseHex(`#708090`),
+  },
+  {
+    name: `snow`,
+    color: Color.parseHex(`#fffafa`),
+  },
+  {
+    name: `springgreen`,
+    color: Color.parseHex(`#00ff7f`),
+  },
+  {
+    name: `steelblue`,
+    color: Color.parseHex(`#4682b4`),
+  },
+  {
+    name: `tan`,
+    color: Color.parseHex(`#d2b48c`),
+  },
+  {
+    name: `teal`,
+    color: Color.parseHex(`#008080`),
+  },
+  {
+    name: `thistle`,
+    color: Color.parseHex(`#d8bfd8`),
+  },
+  {
+    name: `tomato`,
+    color: Color.parseHex(`#ff6347`),
+  },
+  {
+    name: `turquoise`,
+    color: Color.parseHex(`#40e0d0`),
+  },
+  {
+    name: `violet`,
+    color: Color.parseHex(`#ee82ee`),
+  },
+  {
+    name: `wheat`,
+    color: Color.parseHex(`#f5deb3`),
+  },
+  {
+    name: `white`,
+    color: Color.parseHex(`#ffffff`),
+  },
+  {
+    name: `whitesmoke`,
+    color: Color.parseHex(`#f5f5f5`),
+  },
+  {
+    name: `yellow`,
+    color: Color.parseHex(`#ffff00`),
+  },
+  {
+    name: `yellowgreen`,
+    color: Color.parseHex(`#9acd32`),
+  },
+];
+
+export {
+  palette,
+};
