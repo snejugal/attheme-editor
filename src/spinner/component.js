a31e263f34a2e40ba24fd13910921d0cf3769466
SnejUgal <snejugal@icloud.com>
2018-06-26 20:23:19 +0700

    spinner component

---
+++ src/spinner/component.js
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+import React from "react";
+
+class Spinner extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  render () {
+    return <div className="spinner"/>;
+  }
+}
+
+export default Spinner;
