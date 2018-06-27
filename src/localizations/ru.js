04842c2210e7bd9db0db8b57af41da40ec0a20bf
SnejUgal <snejugal@icloud.com>
2018-06-27 20:04:05 +0700

    russian localizatino update

--- src/localizations/ru.js
+++ src/localizations/ru.js
@@ -7,6 +7,8 @@ const NBSP = `\xa0`;
 const pluralRules = new Intl.PluralRules(`ru`);
 
 const localization = {
+  dropHint: () => `Перекиньте сюда файлы .attheme или${NBSP}.attheme-editor `,
+
   error_title: () => `Упс, произошла ошибка`,
   error_description: () => <React.Fragment>
     Пожалуйста, отправьте скриншот ошибки {}
@@ -19,49 +21,57 @@ const localization = {
 
   emptyWorkspace_title: () => `Начните работать над${NBSP}своей темой`,
   emptyWorkspace_createTheme: () => `Создать новую тему`,
-  emptyWorkspace_openTheme: () => `Открыть существующую тему`,
+  emptyWorkspace_openTheme: () => `Открыть существующие темы`,
   emptyWorkspace_credits: () => <React.Fragment>
     Редактор создан {}
     <Link href="//t.me/snejugal">@snejugal</Link>
-    {} и {}
+    {} и&nbsp;
     <Link href="//t.me/AlexStrNik">@AlexStrNik</Link>
-    {} с {}
+    {} с&nbsp;
     <LoveEmoji className="emoji" />.
     {} Посмотрите {}
     <Link href="//github.com/snejugal/attheme-editor">
-      исходный код редактора на GitHub
+      исходный код редактора на&nbsp;GitHub
     </Link>
-    {} и подпишитесь на {}
+    {} и&nbsp;подпишитесь на&nbsp;
     <Link href="//t.me/atthemeeditor">
-      наш канал в Телеграме
+      наш канал в&nbsp;Телеграме
     </Link>!
+  </React.Fragment>,
+  emptyWorkspace_uploadWaysHint: () => <React.Fragment>
+    Также можно перекинуть сюда файл или&nbsp;использовать {}
+    <Link href="//t.me/atthemeeditorbot">@atthemeeditorbot</Link>,
+    {} чтобы открывать темы через&nbsp;Телеграм.
   </React.Fragment>,
 
   theme_defaultName: () => `Классная тема`,
 
   workspace_themeNameLabel: () => `Название темы`,
   workspace_closeTheme: () => `Закрыть тему`,
-  workspace_closeThemePrompt: () => `Вы уверены, что хотите закрыть тему?`,
+  workspace_closeThemePrompt: () => `Вы уверены, что${NBSP}хотите закрыть тему?`,
   workspace_downloadThemeFile: () => `Скачать .attheme напрямую`,
   workspace_createPreview: () => `Создать превью`,
   workspace_testTheme: () => `Протестировать тему`,
   workspace_runScript: () => `Запустить скрипт`,
   workspace_editPalette: () => `Редактировать кастомную палитру темы`,
   workspace_downloadWorkspace: () => `Скачать рабочее окружение`,
-  workspace_unaddedVariable: () => `Не добавлена`,
+  workspace_unaddedVariable: () => `Не${NBSP}добавлена`,
   workspace_unusedVariable: () => `Не используется Телеграмом`,
   workspace_obsoleteVariable: () => `Устаревшая`,
   workspace_nonStandardVariable: () => `Нестандартная`,
   workspace_search: () => `Поиск`,
   workspace_variablesAmount: ({ total, theme }) => {
     const forms = {
-      one: `${theme} переменная из ${total} добавлена в тему`,
-      few: `${theme} переменные из ${total} добавлены в тему`,
-      many: `${theme} переменных из ${total} добавлены в тему`,
+      one: `${theme} переменная из${NBSP}${total} добавлена в${NBSP}тему`,
+      few: `${theme} переменные из${NBSP}${total} добавлены в${NBSP}тему`,
+      many: `${theme} переменных из${NBSP}${total} добавлены в${NBSP}тему`,
     };
 
     return forms[pluralRules.select(theme)];
   },
+  workspace_noVariablesPlaceholder: () => `В${NBSP}теме ещё нет${NBSP}переменных :( Добавьте их${NBSP}через${NBSP}поле поиска выше!`,
+  workspace_noResultsPlaceholder: () => `Упс, поиск не${NBSP}дал никаких результатов. Может, в${NBSP}запросе есть опечатка?`,
+  workspace_uploadError: () => `Упс, редактор не${NBSP}смог загрузить тему боту. Проверьте соединение и${NBSP}попробуйте снова.`,
 
   confirmDialog_yes: () => `Да`,
   confirmDialog_no: () => `Нет`,
@@ -81,20 +91,23 @@ const localization = {
   variableEditor_imageTab: () => `Картинка`,
   variableEditor_colorModelsTab: () => `Цветовые модели`,
   variableEditor_palettesTab: () => `Палитры`,
-  variableEditor_wallpaperColorsHint: () => `Вот несколько цветов из обоев. Нажмите на цвет, чтобы добавить его в палитру темы:`,
+  variableEditor_wallpaperColorsHint: () => `Вот несколько цветов из${NBSP}обоев. Нажмите на${NBSP}цвет, чтобы добавить его в${NBSP}палитру темы:`,
   variableEditor_editPalette: () => `Редактировать палитру`,
+  variableEditor_themeColorsPlaceholder: () => `Упс, в${NBSP}теме ещё нет никаких цветов.`,
+  variableEditor_themeCustomPalettePlaceholder: () => `Упс, в${NBSP}кастомной палитре нет цветов… пока${NBSP}что. Нажмите на кнопку «Редактировать палитру» выше!`,
 
   scriptRunner_title: () => `Запуск скриптов`,
   scriptRunner_description: () => <React.Fragment>
-    .attheme editor позволяет легко запускать скрипты, написанные на EcmaScript 2017, что ускоряет разработку тем. Про API редактора можно узнать в <Link href="//github.com/SnejUgal/attheme-editor/wiki/.attheme-editor-scripts-documentation">вики GitHub репозитория редактора</Link>.
+    .attheme editor позволяет легко запускать скрипты, написанные на&nbsp;EcmaScript 2017, что ускоряет разработку тем. Про API редактора можно узнать в&nbsp;<Link href="//github.com/SnejUgal/attheme-editor/wiki/.attheme-editor-scripts-documentation">вики GitHub репозитория редактора</Link>.
   </React.Fragment>,
   scriptRunner_close: () => `Закрыть`,
   scriptRunner_run: () => `Запустить`,
   scriptRunner_isEvaluated: () => `Скрипт успешно выполнился!`,
-  scriptRunner_runtimeError: () => `Упс, в скрипте произошла ошибка:`,
-  scriptRunner_syntaxError: () => `Упс, в скрипте неверный синтаксис:`,
-  scriptRunner_logMessage: () => `Ваш скрипт передал в консоль:`,
-  scriptRunner_babelLoadingFailed: () => `Не удалось загрузить Babel. Проверьте соединение и перезагрузите страницу.`,
+  scriptRunner_runtimeError: () => `Упс, в${NBSP}скрипте произошла ошибка:`,
+  scriptRunner_syntaxError: () => `Упс, в${NBSP}скрипте неверный синтаксис:`,
+  scriptRunner_logMessage: () => `Ваш скрипт передал в${NBSP}консоль:`,
+  scriptRunner_babelLoadingFailed: () => `Не удалось загрузить Babel. Проверьте соединение и${NBSP}перезагрузите страницу.`,
+  scriptRunner_interpreterLoadingFailed: () => `Не удалось загрузить интерпретатор. Проверьте соединение и${NBSP}перезагрузите страницу.`,
 
   palettes_apple: () => `Apple`,
   palettes_materialDesign: () => `Material Design`,
@@ -106,10 +119,11 @@ const localization = {
   paletteEditor_cancel: () => `Отменить`,
   paletteEditor_save: () => `Сохранить`,
   paletteEditor_delete: () => `Удалить`,
-  paletteEditor_back: () => `Обратно к переменной`,
+  paletteEditor_back: () => `Обратно к${NBSP}переменной`,
   paletteEditor_newColor: () => `Добавить новый цвет`,
   paletteEditor_title: () => `Кастомная палитра темы`,
   paletteEditor_defaultColorName: () => `Красивый цвет`,
+  paletteEditor_placeholder: () => `Упс, палитра пустая… пока${NBSP}что. Нажмите на кнопку «Добавить новый цвет» или, если на${NBSP}chat_wallpaper стоит картинка, откройте редактор этой переменной и${NBSP}добавьте цвета, которые он${NBSP}предлагает!`,
 };
 
 export default localization;
