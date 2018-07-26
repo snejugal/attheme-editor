2aba3f94244d4851c3f864570715303efdbce8bb
SnejUgal <snejugal@icloud.com>
2018-07-26 20:55:45 +0700

    Support `data-` directives and wallpaper in previews

--- src/variable-preview/component.js
+++ src/variable-preview/component.js
@@ -3,8 +3,11 @@ import "./styles.scss";
 import Color from "../color";
 import PropTypes from "prop-types";
 import React from "react";
+import calculateWallpaperSize from "../calculate-wallpaper-size";
 import { defaultValues } from "../attheme-variables";
 import previewsMap from "./previews-map";
+
+const CHANNEL = 255;
 
 const parser = new DOMParser();
 
@@ -26,6 +29,8 @@ class VariablePreview extends React.Component {
   componentDidMount = async () => {
     const previewFileName = previewsMap[this.props.variable];
 
+    const idsMap = {};
+
     if (!previewFileName) {
       return;
     }
@@ -43,15 +48,39 @@ class VariablePreview extends React.Component {
     const coloredElements = Array.from(svg.querySelectorAll(`[class]`));
 
     for (const element of coloredElements) {
+      const currentFill = element.getAttribute(`fill`) || ``;
+
+      if (currentFill.startsWith(`url`) && this.props.currentWallpaper) {
+        const id = currentFill.slice(`url(#`.length, -`)`.length);
+
+        idsMap[id] = element;
+
+        continue;
+      }
+
       const variable = element.getAttribute(`class`);
 
       let color = this.props.theme.variables[variable];
+
+      if (!color && `fallback` in element.dataset) {
+        color = this.props.theme.variables[element.dataset.fallback];
+
+        if (`fallbackAlpha` in element.dataset) {
+          color.alpha = Number(element.dataset.fallbackAlpha) * CHANNEL;
+        }
+      }
 
       if (!color) {
         color = defaultValues[variable];
       }
 
-      element.style.fill = Color.createCssRgb(color);
+      const cssRgb = Color.createCssRgb(color);
+
+      if (element.tagName === `stop`) {
+        element.style.stopColor = cssRgb;
+      } else {
+        element.style.fill = cssRgb;
+      }
     }
 
     const linkElements = Array.from(svg.querySelectorAll(`[href]`));
@@ -60,10 +89,83 @@ class VariablePreview extends React.Component {
       for (const element of linkElements) {
         const url = element.getAttribute(`href`);
 
-        if (!url.startsWith(`data:`)) {
+        if (!url.startsWith(`data:`) && url !== ``) {
           const resolvedUrl = await import(`./previews/${url}`);
 
           element.setAttribute(`href`, resolvedUrl);
+        }
+      }
+    }
+
+    if (this.props.currentWallpaper) {
+      const wallpaperElements = Array.from(
+        svg.querySelectorAll(`[data-wallpaper]`),
+      );
+
+      const wallpaperSize = calculateWallpaperSize(this.props.currentWallpaper);
+      const wallpaperRatio = wallpaperSize.width / wallpaperSize.height;
+
+      if (wallpaperElements.length > 0) {
+        const image = `data:image;base64,${this.props.currentWallpaper}`;
+
+        for (const imageElement of wallpaperElements) {
+          const patternElementId = imageElement.closest(`pattern`).id;
+
+          const element = idsMap[patternElementId];
+
+          // We have to figure out wallpaper element's size on our own
+          // because the preview is not rendered yet
+          let elementWidth = element.getAttribute(`width`);
+          let elementHeight = element.getAttribute(`height`);
+
+          const [svgXOffset, svgYOffset, svgWidth, svgHeight] = svg
+            .getAttribute(`viewBox`)
+            .split(` `)
+            .map(Number);
+
+          if (elementWidth.endsWith(`%`)) {
+            elementWidth = svgWidth;
+          } else {
+            elementWidth = Number(elementWidth);
+          }
+
+          if (elementHeight.endsWith(`%`)) {
+            elementHeight = svgHeight;
+          } else {
+            elementHeight = Number(elementHeight);
+          }
+
+          const elementSizeRatio = elementWidth / elementHeight;
+          const elementX = Number(element.getAttribute(`x`)) || svgXOffset;
+          const elementY = Number(element.getAttribute(`y`)) || svgYOffset;
+
+          let finalWidth;
+          let finalHeight;
+          let similarity;
+
+          if (wallpaperRatio > elementSizeRatio) {
+            finalHeight = 1;
+            finalWidth = wallpaperRatio;
+            similarity = elementHeight;
+          } else {
+            finalWidth = 1;
+            finalHeight = 1 / wallpaperRatio;
+            similarity = elementWidth;
+          }
+
+          finalWidth *= similarity;
+          finalHeight *= similarity;
+
+          // They don't point to the center, these are coords that will place
+          // the image in the center
+          const xCenter = elementWidth / 2 + elementX - finalWidth / 2;
+          const yCenter = elementHeight / 2 + elementY - finalHeight / 2;
+
+          imageElement.setAttribute(`href`, image);
+          imageElement.setAttribute(`width`, finalWidth);
+          imageElement.setAttribute(`height`, finalHeight);
+          imageElement.setAttribute(`x`, xCenter);
+          imageElement.setAttribute(`y`, yCenter);
         }
       }
     }
@@ -87,6 +189,10 @@ class VariablePreview extends React.Component {
     const cssRgb = Color.createCssRgb(this.props.currentColor);
 
     for (const element of this.state.updateeElements) {
+      if (element.tagName === `stop`) {
+        element.style.stopColor = cssRgb;
+      }
+
       element.style.fill = cssRgb;
     }
   };
