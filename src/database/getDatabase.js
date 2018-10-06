43e1fd760a36f91503b5914c2ad69051a6d7ff38
SnejUgal <snejugal@icloud.com>
2018-10-06 21:23:14 +0700

    Simplify database resolver and api implementation

--- src/database/getDatabase.js
+++ src/database/getDatabase.js
@@ -1,45 +1,24 @@
 import upgrade from "./upgrade";
 
-let database;
-
-const awaiters = [];
-
 const errorHandler = ({ target: { error } }) => {
   throw error;
 };
 
-const getDatabase = () => new Promise((resolve) => {
-  if (database) {
-    resolve(database);
-  } else {
-    awaiters.push(resolve);
-  }
+export default new Promise((resolve) => {
+  const request = indexedDB.open(`attheme-editor`, 1);
+
+  request.onsuccess = ({ target: { result } }) => {
+    result.onerror = errorHandler;
+
+    resolve(result);
+  };
+
+  request.onupgradeneeded = async ({ target: { result } }) => {
+    result.onerror = errorHandler;
+
+    await upgrade(result);
+    resolve(result);
+  };
+
+  request.onerror = errorHandler;
 });
-
-const resolveAwaiters = () => {
-  for (const awaiter of awaiters) {
-    awaiter(database);
-  }
-};
-
-const request = indexedDB.open(`attheme-editor`, 1);
-
-request.onsuccess = ({ target: { result } }) => {
-  database = result;
-  database.onerror = errorHandler;
-
-  resolveAwaiters();
-};
-
-request.onupgradeneeded = async ({ target: { result } }) => {
-  database = result;
-  database.onerror = errorHandler;
-
-  await upgrade(database);
-
-  resolveAwaiters();
-};
-
-request.onerror = errorHandler;
-
-export default getDatabase;
