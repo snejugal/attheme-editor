97d9de68b31741bced3f9aabfaf7df58d52e7256
SnejUgal <snejugal@icloud.com>
2018-11-02 16:28:41 +0700

    Rewrite `CodeEditor` in TS

--- src/types.d.ts
+++ src/types.d.ts
@@ -15,3 +15,28 @@ interface Theme {
   wallpaper?: string;
   palette: (string | PartialColor)[];
 }
+
+// This is a workaround, because Babel (that CRA uses) doesn't support `import`
+// in type declarations.
+// https://github.com/babel/babel/pull/8798
+class CodeMirrorClass {
+  constructor(host: HTMLElement, options: {
+    value: string;
+    lineNumbers: boolean;
+    autofocus: boolean;
+    mode: string;
+    indentUnit: number;
+    indentWithTabs: boolean;
+    tabSize: number;
+  });
+
+  on(event: string, handler: () => any);
+
+  getValue(): string;
+}
+
+declare module "codemirror/lib/codemirror.css" { }
+declare module "codemirror/mode/javascript/javascript.js" { }
+declare module "codemirror" {
+  export default CodeMirrorClass;
+}
