83a36db607495a7bbc3a9d72c783bbbcdcefe824
SnejUgal <snejugal@icloud.com>
2018-11-05 20:22:46 +0700

    Add new types

--- src/types.d.ts
+++ src/types.d.ts
@@ -25,27 +25,63 @@ interface Theme {
   palette: Palette;
 }
 
-// This is a workaround, because Babel (that CRA uses) doesn't support `import`
-// in type declarations.
+// `typeof import(...)` is unsupported by Babel, so we declare a global type in
+// a file that it doesn't parse.
 // https://github.com/babel/babel/pull/8798
-class CodeMirrorClass {
-  constructor(host: HTMLElement, options: {
-    value: string;
-    lineNumbers: boolean;
-    autofocus: boolean;
-    mode: string;
-    indentUnit: number;
-    indentWithTabs: boolean;
-    tabSize: number;
-  });
-
-  on(event: string, handler: () => any);
-
-  getValue(): string;
-}
+type CodeMirrorClass = typeof import("codemirror");
+type Babel = typeof import("@babel/standalone");
+type EsInterpreter = import("es-interpreter");
+type EsInterpreterInstance = import("es-interpreter").default;
+type EsInterpreterScope = import("es-interpreter").Scope;
+type VibrantClass = typeof import("node-vibrant").default;
+type Quantizer = typeof import("node-vibrant/lib/quantizer/worker").default;
 
 declare module "codemirror/lib/codemirror.css" { }
 declare module "codemirror/mode/javascript/javascript.js" { }
-declare module "codemirror" {
-  export default CodeMirrorClass;
+
+declare module "@babel/standalone" {
+  interface BabelOptions {
+    presets?: string[];
+    plugins?: string[];
+  };
+
+  export function transform(code: string, options: BabelOptions): {
+    code: string,
+  };
+}
+declare module "es-interpreter" {
+  export interface Scope { };
+  export interface Peudo { };
+  export interface Descriptor { };
+
+
+  export default class Interpeter {
+    static READONLY_DESCRIPTOR: Descriptor = {};
+
+    constructor(code: string, prepare: (interpeter: Interpeter, scope: Scope) => void);
+
+    setProperty(
+      scope: Scope,
+      identifier: string,
+      value: Peudo,
+      descriptor?: Descriptor,
+    );
+    nativeToPseudo(value: any): Peudo;
+    createNativeFunction(
+      fn: <T extends unknown[]>(...args: T) => unknown,
+    ): Peudo;
+    pseudoToNative(value: Pseudo): unknown;
+    step(): boolean;
+  }
+}
+
+declare module "fuzzy-search" {
+  export default class FuzzySearch {
+    constructor(hay: string[], keys: strings[], options: {
+      caseSensetive?: boolean,
+      sort?: boolean,
+    });
+
+    search(needle: string): string[];
+  };
 }
